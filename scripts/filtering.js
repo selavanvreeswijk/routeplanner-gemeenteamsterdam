4640f6ea247659a31d30ce5708c59025f26b5098
Louisemonte03 <louisemonte03@gmail.com>
2026-06-02 11:30:27 +0200

    added animation + darkmode
    Hier heb ik kleine subtiele animaties toegevoegd. Ook de darkmode is toegevoegd

--- scripts/filtering.js
+++ scripts/filtering.js
@@ -1,24 +1,24 @@
 export function countFilter(feature, filterGroups) {
-	const properties = feature.properties;
-	const count = {};
+  const properties = feature.properties;
+  const count = {};
 
-	for (const [filter, keys] of Object.entries(filterGroups)) {
-		// maak van de filterGroups paren van de keys and values, dus bijv ["prikkels", ["Prikkelarme_ruimte", "Prikkelarme_uren"]]
-		count[filter] = { Ja: 0, Nee: 0, Onbekend: 0 };
+  for (const [filter, keys] of Object.entries(filterGroups)) {
+    // maak van de filterGroups paren van de keys and values, dus bijv ["prikkels", ["Prikkelarme_ruimte", "Prikkelarme_uren"]]
+    count[filter] = { Ja: 0, Nee: 0, Onbekend: 0 };
 
-		for (const key of keys) {
-            // pak de values van elke key
-			const filterValue = properties[key];
+    for (const key of keys) {
+      // pak de values van elke key
+      const filterValue = properties[key];
 
-			if (filterValue !== undefined) {
-				const value = filterValue.trim();
+      if (filterValue !== undefined) {
+        const value = filterValue.trim();
 
-				if (value in count[filter]) {
-					count[filter][value]++;
-				}
-			}
-		}
-	}
-	console.log(count)
-	return count;
+        if (value in count[filter]) {
+          count[filter][value]++;
+        }
+      }
+    }
+  }
+  console.log(count);	
+  return count;
 }
